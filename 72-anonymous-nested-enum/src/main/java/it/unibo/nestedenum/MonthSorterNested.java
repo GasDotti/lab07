fe73b1f8712450f38125f84d2e1475c3309ea5aa
Castorri Lorenzo <loricastorri@gmail.com>
2025-11-09 11:51:19 +0100

    72: Created 'enum Months' in final class MonthSorterNested

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -18,4 +18,43 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    public enum Months {
+        
+        JANUARY (1, "january", 31),
+        FEBRUARY (2, "february", 28),
+        MARCH (3, "march", 31),
+        APRIL (4, "april", 30),
+        MAY (5, "may", 31),
+        JUNE (6, "june", 30),
+        JULY (7, "july", 31),
+        AUGUST(8, "august", 31),
+        SEPTEMBER(9, "september", 30),
+        OCTOBER(10, "october", 31),
+        NOVEMBER(11, "november", 30),
+        DECEMBER(12, "december", 31);
+
+        private final int id;
+        private final String finalName;
+        private final int days;
+
+        private Months(final int id, final String finalName, final int days){
+            this.id = id;
+            this.finalName = finalName;
+            this.days = days;
+        }
+        public static Months FromString (String name){
+            for (Months m : Months.values()){
+                if(m.equals(name)){
+                    return m;
+                }
+            }
+            return null;
+        }
+        @Override
+        public String toString() {
+            return this.finalName;
+        }
+
+    }
 }
